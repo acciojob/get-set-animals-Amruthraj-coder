56a4ed98ca19bdcfa36e946de70009464958d10d
Amruthraj-coder <122180037+Amruthraj-coder@users.noreply.github.com>
2023-07-22 14:06:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,64 @@
-//complete this code
+// //complete this code
+// class Animal {
+// 	 _species;
+//   constructor(species){
+//     this._species=species;
+//   }
+//   get species() {
+//     return this._species;
+//   }
+
+//   makeSound() {
+//     console.log("Animal sound"); // We'll implement this method in the subclasses
+//   }
+
+// }
+
+// class Dog extends Animal {
+// 	 bark(){
+//     console.log('The Golden Retriever makes a sound')
+//   }
+
+// 	 makeSound() {
+//     console.log("woof");
+//   }
+// }
+
+// class Cat extends Animal {
+// 	 purr(){
+//     console.log('The Siamese makes a sound')
+//   }
+// 	 makeSound() {
+//     console.log("Meow!");
+//   }
+// }
+
+// // Do not change the code below this line
+// window.Animal = Animal;
+// window.Dog = Dog;
+// window.Cat = Cat;
 class Animal {
-	 _species;
-  constructor(species){
-    this._species=species;
-  }
-  get species() {
-    return this._species;
-  }
-
-  makeSound() {
-    console.log("Animal sound"); // We'll implement this method in the subclasses
-  }
-
+	constructor(someSpecies){
+		this._species = someSpecies;
+	}
+	get species(){
+	   return this._species;
+	}
+    makeSound(){
+		console.log(`The ${this.species} makes a sound`);
+	}
 }
 
 class Dog extends Animal {
-	 bark(){
-    console.log('The Golden Retriever makes a sound')
-  }
-
-	 makeSound() {
-    console.log("woof");
-  }
+	bark(){
+		console.log("woof");
+	}
 }
 
 class Cat extends Animal {
-	 purr(){
-    console.log('The Siamese makes a sound')
-  }
-	 makeSound() {
-    console.log("Meow!");
-  }
+	purr(){
+		console.log("purr");
+	}
 }
 
 // Do not change the code below this line
