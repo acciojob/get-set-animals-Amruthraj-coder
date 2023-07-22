dbb7cb10d6712d3a94778a22bae10918c1e2aed6
Amruthraj-coder <122180037+Amruthraj-coder@users.noreply.github.com>
2023-07-22 13:53:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 //complete this code
-class Animal {}
+class Animal {
+	 _species;
+  constructor(species){
+    this._species=species;
+  }
+  get makeSound(){
+    console.log(this._species)
+  }
 
-class Dog extends Animal {}
+}
 
-class Cat extends Animal {}
+class Dog extends Animal {
+	 bark(){
+    console.log("woof")
+  }
+}
+
+class Cat extends Animal {
+	 purr(){
+    console.log("purr")
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
